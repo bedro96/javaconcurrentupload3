417ef2bf40d2aa04e7766a39170d8a4237209003
bedro96@naver.com <bedro96@naver.com>
2019-12-04 08:21:41 +0000

    Taking storage key out

--- src/main/java/com/sec/scloud/file/Config.java
+++ src/main/java/com/sec/scloud/file/Config.java
@@ -8,6 +8,6 @@ public class Config {
 
     public static final int TIMEOUT_MAX = 120000;
 
-    public static final String CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=javaconcurrentstg;AccountKey=sD3lNqoiIQEYUDKJH5VWttUvAnE7jaabGpdm79sFDWFHTWAzTyBUSWLYFhV+aaXI4pyV4eHPixQtid8RUBE1vQ==;EndpointSuffix=core.windows.net";
+    public static final String CONNECTION_STRING = "";
     public static final String CONTAINER_NAME = "javaconcurrentblockblob";
 }
